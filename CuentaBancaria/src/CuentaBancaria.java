be5514e6aee33e60a837d158f4655892db992e8c
Ainhoa Martínez Fernández <a25ainhoamf@teis.intranet>
2025-10-01 21:42:09 +0200

    solucion

--- CuentaBancaria/src/CuentaBancaria.java
+++ CuentaBancaria/src/CuentaBancaria.java
@@ -8,7 +8,6 @@ public class CuentaBancaria {
 //     Atributos (siempre privados)
 
     private double saldo = 0.0;
-    private double money;
 
 //    Getters y Setters (clic derech > Generate > getters)
 
@@ -20,21 +19,13 @@ public class CuentaBancaria {
         this.saldo = saldo;
     }
 
-    public double getMoney() {
-        return money;
-    }
 
-    public void setMoney(double money) {
-        this.money = money;
-    }
 
 //    Constructores
 
-    public CuentaBancaria(double saldo, double money) {
-        this.saldo = saldo;
-        this.money = money;
+    public CuentaBancaria(double saldo, double moneyToAdd, double moneyToTake) {
+        this.saldo = 0;
     }
-
 
 //    Funciones
 
@@ -43,17 +34,18 @@ public class CuentaBancaria {
 //Desde un menú el usuario podrá depositar una cantidad de dinero y retirar dinero de la cuenta.
 //El banco no permite que la cuenta baje de 0€.
 
-    public double takeMoney() {
-        if (saldo != 0.0) {
-            return this.money = this.saldo - this.money;
-        } else {
-            System.out.println("Saldo insuficiente");
+    public double takeMoney(double money) {
+        if(this.saldo - money > 0){
+            this.saldo = this.saldo - money;
+            return this.saldo;
+        }else{
+            return -1;
         }
-        return saldo;
     }
 
-    public double addMoney() {
-        return this.saldo = this.saldo + this.money;
+    public double addMoney(double money) {
+        this.saldo = this.saldo + money;
+        return this.saldo;
     }
 }
 //El banco también ha de mostrar desde el menú la información de la cuenta.
--- CuentaBancaria/src/Program.java
+++ CuentaBancaria/src/Program.java
@@ -1,17 +1,48 @@
 import java.util.Scanner;
 
 public class Program {
+
+    final static int OP_INGRESA =1, RETIRA = 2, SALDO = 3, SALIR = 4;
+
     public static void main(String[] args) {
         //El banco también ha de mostrar desde el menú la información de la cuenta.
 
-        System.out.println("¡Hola! Tienes " + double saldo + " euros en tu saldo, que quieres hacer, depositar dinero (1) o sacar dinero (2)");
-        Scanner sc = new Scanner(System.in);
-        double inputUser = sc.nextDouble();
+        CuentaBancaria cb = new CuentaBancaria();
 
-        if (inputUser = 1) {
-        } else if (inputUser = 2) {
-            System.out.println("Ahora tu saldo es " + double addMoney + " euros");
+        while(true){
+            System.out.println("¡Hola! Tienes " + double saldo + " euros en tu saldo, que quieres hacer?);
+            System.out.println("\t1.Ingresa");
+            System.out.println("\t2.Retira");
+            System.out.println("\t3.Saldo");
+            System.out.println("\t4.Salir");
+
+            Scanner sc = new Scanner(System.in);
+            int userOption = sc.nextInt();
+            double saldo = 0;
+
+            if(userOption == OP_INGRESA){
+                System.out.println("Introduce el dinero a ingresar");
+                double money = sc.nextDouble();
+                saldo = cb.addMoney(money);
+            }
+
+            if(userOption == RETIRA){
+                System.out.println("Introduce el dinero a ingresar");
+                double money = sc.nextDouble();
+                saldo = cb.takeMoney(money);
+                if(saldo == -1){
+                    System.out.println("Saldo insuficiente");
+                }
+            }
+
+            if(userOption == SALDO){
+                saldo = cb.getSaldo();
+            }
+            if(userOption == SALIR){
+                System.out.println("Adios");
+                break;
+            }
+            System.out.println("El saldo actual es: " + saldo);
         }
-
     }
 }
